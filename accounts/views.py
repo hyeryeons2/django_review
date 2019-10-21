45f95b5fb2b7d407c64d5b7e1419618dff496525
Hailey <hyeryeons23@gmail.com>
2019-10-21 17:30:08 +0900

    Finish Authentication, Apply bootstrap

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # as auth_login으로 하는 이유? import login과 def login 이름이 겹침
 from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.views.decorators.http import require_POST
+
 
 def signup(request):
+    # 만약에 user가 인증 상태에서 sign 요청 보내면,
+    if request.user.is_authenticated:
+        return redirect('articles:index')
     if request.method == 'POST':
         # 회원가입 로직
         # get에서 보낸 데이터 from을 post로 받고
@@ -12,6 +17,9 @@ def signup(request):
         # 유효성 검사후, 통과하면 저장하기
         if form.is_valid():
             form.save()
+            # 회원가입하면 자동 로그인 상태로 보이는 페이지
+            user = form.save()
+            auth_login(request, user)
             return redirect('articles:index')
     else:
         # usercreationform을 context로 저장해서 signup html에서 보여준다
@@ -26,6 +34,8 @@ def signup(request):
 
 # 로그인은 session data 한 줄을 만들어 주는 것
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
     # post 로그인 시켜준다
     if request.method == 'POST':
         # 주의!! AuthenticationForm은 requst, request.POST로 내야함
@@ -33,7 +43,13 @@ def login(request):
         if form.is_valid():
             # 로그인 시키는 과정
             auth_login(request, form.get_user())
-            return redirect('articles:index')
+
+            # ★★★★login required 때문에 next page로 보내짐
+            # ★★★★next_page가 있으면 next page로 보내고 아니면 index 페이지로 보내세요
+            # login 끝나면 create로 보내세요
+            next_page = request.GET.get('next')
+            #redirect는 get method(요청)에만 가능
+            return redirect(next_page or 'articles:index')
         # invalid 한 경우, 그냥 context에 form을 넣어서 보내줌
     else: # get 로그인 할 수 있는 페이지 제공
         form = AuthenticationForm()
@@ -43,4 +59,11 @@ def login(request):
 
 def logout(request):
     auth_logout(request)
+    return redirect('articles:index')
+
+
+@require_POST
+# 유저 삭제(회원탈퇴)
+def delete(request):
+    request.user.delete()
     return redirect('articles:index')
--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404  # redirect 추가
 from django.views.decorators.http import require_POST, require_GET
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .forms import ArticleForm, CommentForm
 from .models import Article, Comment
-# from IPython import embed 
+from IPython import embed 
 
-# Create your views here.
 
 @require_GET
 def index(request):
+    # embed()
     articles = Article.objects.all()
     context = {'articles': articles}
     return render(request, 'articles/index.html', context)
@@ -33,7 +35,8 @@ def detail(request, article_pk):
     # return render(request, 'article/detail.html', {'article': article})
     # 이렇게 나와도 전혀 상관없음(틀린게 아님!)
 
-
+# 로그인 된 상태에서만 CREATE 실행
+@login_required
 def create(request):
     # 만약 사용자가 보낸 method가 POST(Article을 생성해달라고 하는 요청)
     if request.method == 'POST':
@@ -51,6 +54,7 @@ def create(request):
     context = {'form': form}
     return render(request, 'articles/create.html', context)
 
+@login_required  # get 요청 있는 경우에만 login_required 가능
 # /articles/3/update
 def update(request, article_pk):
     # 3번 article을 update할 때 있으면 수정, 아니면 404 보여주기
@@ -74,38 +78,51 @@ def update(request, article_pk):
     context = {'form': form}
     return render(request, 'articles/update.html', context)
 
+# login_required 는 get 요청에만 가능하므로 POST에는 쓸 수 없음
+# @login_required
 # /articles/3/delete/
 @require_POST
 def delete(request, article_pk):
     # 함부로 delete 하지 못하게 하기 위해 POST method 해줘야 함
 
+    if request.user.is_authenticated:
     # article_pk에 맞는 article을 꺼내서 삭제함
     # 삭제할 article 꺼내는 작업
-    article = get_object_or_404(Article, pk=article_pk)
+        article = get_object_or_404(Article, pk=article_pk)
     # article 이 있다면(no 404 page) 삭제
-    article.delete()
+        article.delete()
     return redirect('articles:index')
 
+
 # detail 페이지에서 다 처리할 것이기 때문에 get이 따로 없으므로
-# require_POST 처리함    
+# require_POST 처리함
+# @login_required ; post ㄴㄴ 
 @require_POST
 def comments_create(request, article_pk):
-    article = get_object_or_404(Article, pk=article_pk)
-    form = CommentForm(request.POST)
-    if form.is_valid():
-        # 완전히 db에 반영하지는 말자 commit=False(임시저장)
-        comment = form.save(commit=False)
-        comment.article_id = article.pk
-        # 이 작업이 끝나면 comment에 content와 id가 다 들어가고, 그걸 저장한다
-        comment.save()
-    # 유효하든, 유효하지않든 이 화면 보여준다
-    return redirect('articles:detail', article.pk)
+    if request.user.is_authenticated:
+        form = CommentForm(request.POST)
 
+        if form.is_valid():
+            # 완전히 db에 반영하지는 말자 commit=False(임시저장)
+            comment = form.save(commit=False)
+            comment.article_id = article_pk
+            # 이 작업이 끝나면 comment에 content와 id가 다 들어가고, 그걸 저장한다
+            comment.save()
+        # 유효하든, 유효하지않든 이 화면 보여준다
+    return redirect('articles:detail', article_pk)
+
+
+# @login_required
 @require_POST
-def comments_delete(request, comment_pk):
-    comment = get_object_or_404(Comment, pk=comment_pk)
-    article_pk = comment.article_id
-    comment.delete()
+def comments_delete(request, article_pk, comment_pk):
+    if request.user.is_authenticated:
+        comment = get_object_or_404(Comment, pk=comment_pk)
+        comment.delete()
     # article = get_object_or_404(Article, pk=article_pk) 안했으니까
     # article.pk 못씀
-    return redirect('articles:detail', article_pk)
+        return redirect('articles:detail', article_pk)
+        
+    # 401 code == 인증되지 않았다
+    # 이 설정 후, 쿠키>session 삭제하면, 로그아웃이 되고, 그 상태에서 댓글 삭제를 하면 
+    # 401 페이지와 함께 you are Unauthorized 가 출력됨
+    return HttpResponse('You are Unauthorized', status=401)
